c1bb1a05e83b84e20bcb35e886aff88be33c7279
马慧莹 <408874626@qq.com>
2017-08-30 19:14:37 +0800

    更改名称

---
+++ NetworkRequestTest/NetworkRequestTest/MNetwork/MNetRequestModel.m
@@ -0,0 +1,90 @@
+//
+//  MNetRequestModel.m
+//  268EDU_Demo
+//
+//  Created by yizhilu on 2017/7/20.
+//  Copyright © 2017年 edu268. All rights reserved.
+//
+
+#import "MNetRequestModel.h"
+
+@implementation MNetRequestModel
+
++ (void)netRequestSeting:(MNetSetting *)seting success:(void (^)(id responseData))success failure:(void (^)(NSError *error))failure{
+    if ([MNetSetting isNoNet]) {
+        [MBProgressHUD showMBPAlertView:@"当前网络不可用" withSecond:1.5];
+        return;
+    }
+   
+    [self printRequestUrlString:seting.hostUrl withParamter:seting.paramet];
+    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+    manager.responseSerializer = [AFJSONResponseSerializer serializer];
+    [manager.requestSerializer setTimeoutInterval:10.f];
+    manager.responseSerializer.acceptableContentTypes = [NSSet setWithObjects:@"application/json", @"text/json", @"text/javascript",@"text/html", nil];
+    if (seting.isHttpsRequest) {
+        NSString *cerPath = [[NSBundle mainBundle] pathForResource:@"nideke" ofType:@"cer"];
+        NSData * certData =[NSData dataWithContentsOfFile:cerPath];
+        NSSet * certSet = [[NSSet alloc] initWithObjects:certData, nil];
+        AFSecurityPolicy *securityPolicy = [AFSecurityPolicy policyWithPinningMode:AFSSLPinningModeCertificate];
+        // 是否允许,NO-- 不允许无效的证书
+        [securityPolicy setAllowInvalidCertificates:NO];
+        //设置证书
+        [securityPolicy setValidatesDomainName:YES];
+        [securityPolicy setPinnedCertificates:certSet];
+        manager.securityPolicy = securityPolicy;
+    }
+    
+    if (seting.requestStytle == MRequestMethodPOST) {
+        [manager POST:seting.hostUrl parameters:seting.paramet progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject){
+            NSHTTPURLResponse* httpResponse = (NSHTTPURLResponse*)task.response;
+            NSInteger responseStatusCode = [httpResponse statusCode];
+            if (success != nil && responseStatusCode == 200){
+                success(responseObject);
+            }
+            
+        }failure:^(NSURLSessionDataTask * _Nonnull task, NSError * _Nonnull error){
+            
+            if (failure != nil)
+            {
+                failure(error);
+            }
+        }];
+        
+    }else{
+        [manager GET:seting.hostUrl parameters:seting.paramet progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject){
+            NSHTTPURLResponse* httpResponse = (NSHTTPURLResponse*)task.response;
+            NSInteger responseStatusCode = [httpResponse statusCode];
+            if (success != nil && responseStatusCode == 200){
+                success(responseObject);
+            }
+        }failure:^(NSURLSessionDataTask * _Nonnull task, NSError * _Nonnull error){
+            
+            if (failure != nil)
+            {
+                failure(error);
+            }
+        }];
+    }
+    
+}
+
+
++ (void)printRequestUrlString:(NSString *)urlString withParamter:(NSDictionary *)dic
+{
+    NSArray *dicKeysArray = [dic allKeys];
+    NSString *urlWithParamterString = urlString;
+    if (dicKeysArray.count != 0){
+        urlWithParamterString = [urlWithParamterString stringByAppendingString:@"?"];
+    }
+    for (NSInteger i = 0; i < dicKeysArray.count; i++){
+        
+        urlWithParamterString = [urlWithParamterString stringByAppendingString:[NSString stringWithFormat:@"%@=%@&", dicKeysArray[i], [dic objectForKey:dicKeysArray[i]]]];
+        if (i == dicKeysArray.count - 1){
+            urlWithParamterString = [urlWithParamterString substringToIndex:urlWithParamterString.length - 1];
+        }
+    }
+    
+    NSLog(@"\n\n路径--%@", urlWithParamterString);
+}
+
+@end
---
+++ NetworkRequestTest/NetworkRequestTest/MNetwork/MNetworkUtils.m
@@ -0,0 +1,103 @@
+//
+//  MNetworkUtils.m
+//  268EDU_Demo
+//
+//  Created by yizhilu on 2017/7/21.
+//  Copyright © 2017年 edu268. All rights reserved.
+//
+
+#import "MNetworkUtils.h"
+#import <CommonCrypto/CommonDigest.h>
+
+@implementation MNetworkUtils
+//md5加密
++ (NSString *)md5StringFromString:(NSString *)string {
+    NSParameterAssert(string != nil && [string length] > 0);
+    
+    const char *value = [string UTF8String];
+    unsigned char outputBuffer[CC_MD5_DIGEST_LENGTH];
+    CC_MD5(value, (CC_LONG)strlen(value), outputBuffer);
+    NSMutableString *outputString = [[NSMutableString alloc] initWithCapacity:CC_MD5_DIGEST_LENGTH * 2];
+    for(NSInteger count = 0; count < CC_MD5_DIGEST_LENGTH; count++){
+        [outputString appendFormat:@"%02x",outputBuffer[count]];
+    }
+    
+    return outputString;
+}
+
+//如果没达到指定日期返回-1，刚好是这一时间，返回0，否则返回1
++ (NSInteger)compareCurrentTime:(NSDate *)currentTime withFileCreatTime:(NSDate *)fileCreatTime
+{
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    [dateFormatter setDateFormat:@"dd-MM-yyyy-HHmmss"];
+    NSString *oneDayStr = [dateFormatter stringFromDate:currentTime];
+    NSString *anotherDayStr = [dateFormatter stringFromDate:fileCreatTime];
+    NSDate *dateA = [dateFormatter dateFromString:oneDayStr];
+    NSDate *dateB = [dateFormatter dateFromString:anotherDayStr];
+    NSComparisonResult result = [dateA compare:dateB];
+    NSLog(@"currentTime : %@, fileCreatTime : %@", currentTime, fileCreatTime);
+    NSInteger aa = 0;
+    if (result == NSOrderedDescending) {
+        //文件创建时间超过当前时间,刷新数据
+        aa = 1;
+    }
+    else if (result == NSOrderedAscending){
+        //文件创建时间小于当前时间,返回缓存数据
+        aa = -1;
+    }
+    //NSLog(@"Both dates are the same");
+    return aa;
+    
+}
+
+//json字段检验
++ (BOOL)validateJSON:(id)json withValidator:(id)jsonValidator {
+    if ([json isKindOfClass:[NSDictionary class]] &&
+        [jsonValidator isKindOfClass:[NSDictionary class]]) {
+        NSDictionary * dict = json;
+        NSDictionary * validator = jsonValidator;
+        BOOL result = YES;
+        NSEnumerator * enumerator = [validator keyEnumerator];
+        NSString * key;
+        while ((key = [enumerator nextObject]) != nil) {
+            id value = dict[key];
+            id format = validator[key];
+            if ([value isKindOfClass:[NSDictionary class]]
+                || [value isKindOfClass:[NSArray class]]) {
+                result = [self validateJSON:value withValidator:format];
+                if (!result) {
+                    break;
+                }
+            } else {
+                if ([value isKindOfClass:format] == NO &&
+                    [value isKindOfClass:[NSNull class]] == NO) {
+                    result = NO;
+                    break;
+                }
+            }
+        }
+        return result;
+    } else if ([json isKindOfClass:[NSArray class]] &&
+               [jsonValidator isKindOfClass:[NSArray class]]) {
+        NSArray * validatorArray = (NSArray *)jsonValidator;
+        if (validatorArray.count > 0) {
+            NSArray * array = json;
+            NSDictionary * validator = jsonValidator[0];
+            for (id item in array) {
+                BOOL result = [self validateJSON:item withValidator:validator];
+                if (!result) {
+                    return NO;
+                }
+            }
+        }
+        return YES;
+    } else if ([json isKindOfClass:jsonValidator]) {
+        return YES;
+    } else {
+        return NO;
+    }
+}
+
+
+
+@end
